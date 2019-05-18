609875dc4abae37a019cacd3952dfcf888e46d95
Samielleuch <Sami.Elleuch@insat.u-carthage.tn>
2019-05-18 04:54:58 +0100

    added menu

---
+++ GameState.java
@@ -0,0 +1,10 @@
+public abstract class GameState {
+    //needs refrence so it can change state
+
+    protected GameStateManager gsm ;
+    public abstract void init();
+    public abstract void update();
+    public abstract void draw(java.awt.Graphics2D g);
+    public abstract void keyPressed(int k);
+    public abstract void keyReleased(int k);
+}
---
+++ MenuState.java
@@ -0,0 +1,106 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public class MenuState extends GameState {
+    private Background bg;
+    //to keep track of the selected option
+    private int currentChoice = 0 ;
+    private String[] options= {
+      "Start",
+      "Help",
+      "Quit"
+    };
+private Color titleColor;
+private Font titleFont;
+private Font font;
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void update() {
+     bg.update();
+    }
+
+    @Override
+    public void draw(Graphics2D g) {
+     //draw background
+        bg.draw(g);
+
+        //draw title
+        g.setColor(titleColor);
+        g.setFont(titleFont);
+        g.drawString("Asma game ",80,70);
+        // draw menu options
+        g.setFont(font);
+        for (int i = 0; i < options.length ; i++) {
+            if (i == currentChoice ) {
+                g.setColor(Color.BLACK);
+            }else {
+                g.setColor(Color.RED);
+            }
+            // draw them binethom 15 pixel
+            g.drawString(options[i],145,140 + i * 15 );
+        }
+    }
+
+    private void select() {
+        if(currentChoice == 0 ){
+            //start
+        }
+        if(currentChoice == 1 ){
+            //Help
+        }
+        if(currentChoice == 2 ){
+            //quit
+            System.exit(0);
+        }
+
+    }
+    @Override
+    public void keyPressed(int k) {
+
+         if (k == KeyEvent.VK_ENTER) {
+             select();
+         }
+           if (k == KeyEvent.VK_UP) {
+             //move up
+               currentChoice--;
+             //if we reach the top end go to the last
+               if (currentChoice == -1){
+                   currentChoice = options.length -1 ;
+               }
+         }
+    if (k == KeyEvent.VK_DOWN) {
+        //move down
+        currentChoice++;
+        //if we reach the end end go to the top
+        if (currentChoice == options.length ){
+            currentChoice = 0 ;
+        }
+         }
+
+    }
+
+    @Override
+    public void keyReleased(int k) {
+
+    }
+
+    public MenuState(GameStateManager gsm) {
+        this.gsm = gsm;
+    try {
+
+        bg = new Background("/Resources/Background/menubg.gif",1);
+        //moves to the left
+        bg.setVector(-0.1,0);
+        titleColor = new Color(128,0,0);
+        titleFont = new Font ("Century Gothic",Font.PLAIN , 28 );
+        font = new Font ("Arial",Font.PLAIN , 12 );
+
+    }catch(Exception e) {
+        e.printStackTrace();
+    }
+    }
+}
